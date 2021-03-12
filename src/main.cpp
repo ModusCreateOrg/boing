0a2b6391154704e6cdff9840c285b677a658d23b
Jay Garcia <jesusdgarcia@gmail.com>
2021-03-12 16:09:44 -0500

    fixed main

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@ TOptions *gOptions;
 #include "NetworkDisplay/NetworkDisplay.h"
 #endif
 
-
 // app_main
 extern "C" void app_main(char *fileName) {
 
@@ -30,7 +29,7 @@ extern "C" void app_main(char *fileName) {
 int main(int argc, char *argv[]) {
 #ifdef __MODUS_TARGET_NETWORK_DISPLAY__
   if (argc < 2) {
-    printf("Usage: ./%s <path.to.ini.file>\n", argv[1]);
+    printf("Usage: %s <path.to.ini.file>\n", argv[0]);
     return 1;
   }
 #endif
@@ -38,3 +37,4 @@ int main(int argc, char *argv[]) {
     app_main(argv[1]);
     return 0;
 }
+
