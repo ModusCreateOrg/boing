9679d835cb336962f9e1dd487c5e82aaaaca129c
Jay Garcia <jesusdgarcia@gmail.com>
2021-03-12 15:46:35 -0500

    Added network display capabilities

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,33 @@ GGame *gGame;
 TOptions *gOptions;
 #endif
 
+#ifdef __MODUS_TARGET_NETWORK_DISPLAY__
+#include "NetworkDisplay/NetworkDisplay.h"
+#endif
+
+
 // app_main
-extern "C" void app_main() {
-  gGame = new GGame();
-  gGame->Run();
-  delete gGame;
+extern "C" void app_main(char *fileName) {
+
+    gGame = new GGame();
+#ifdef __MODUS_TARGET_NETWORK_DISPLAY__
+    NetworkDisplay *display = (NetworkDisplay*)&gDisplay;
+//    const char *fileName = "/home/jgarcia/modite-rgb.ini";
+    display->ConfigureFromIniFile(fileName);
+#endif
+    gGame->Run();
+    delete gGame;
 }
 
-#ifndef __XTENSA
-int main() {
-  app_main();
-  return 0;
-}
+
+int main(int argc, char *argv[]) {
+#ifdef __MODUS_TARGET_NETWORK_DISPLAY__
+  if (argc < 2) {
+    printf("Usage: ./%s <path.to.ini.file>\n", argv[1]);
+    return 1;
+  }
 #endif
+
+    app_main(argv[1]);
+    return 0;
+}
